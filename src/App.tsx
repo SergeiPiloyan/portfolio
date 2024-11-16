44aee7b2ad46b5e78418d7ba6bae998d450cf216
Sergei_Piloyan <sergei.piloyan0302@gmail.com>
2024-11-15 17:43:27 -0800

    add general styles and faviocn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Box } from "@mui/material";
+
+const styles = {
+  background: "blue",
+  width: "100%",
+  height: "100%",
+};
+
 const App = () => {
-  return <>My Portfolio</>;
+  return <Box sx={styles}>My Portfolio</Box>;
 };
 
 export default App;
